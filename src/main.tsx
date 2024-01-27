851edeb77af33bd280cad546fb25afe956a4b11e
lLuan Portugal <luanportugal.profissional.tec@gmail.com>
2024-01-27 12:53:10 -0300

    feat(chore): adding system and route protection

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ProtectedRoute from './routes'
 import App from './components/login'
+import RegisterUser from './components/register'
+import FileUpload from './components/upload'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+
+        <Route path="/upload" element={
+          <ProtectedRoute>
+            <FileUpload />
+          </ProtectedRoute>
+        } />
+
+        <Route path="/register" element={<RegisterUser />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
