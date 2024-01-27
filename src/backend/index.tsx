adc0ccd429f5903e68d7ce17a3635988c4ed9de3
lLuan Portugal <luanportugal.profissional.tec@gmail.com>
2024-01-27 12:48:43 -0300

    fix(chore): adding typing and function to register user

--- src/backend/index.tsx
+++ src/backend/index.tsx
@@ -1,7 +1,21 @@
 import axios from "axios";
 
+type CheckUser = {
+    username: string;
+    password: string;
+}
 
-export async function CheckLogin(username: string, password: string) {
+type RegisterUser = {
+    username: string;
+    email: string;
+    password: string;
+    confirmPassword?: string;
+}
+
+
+export async function checkLogin(params:CheckUser) {
+
+    const { username, password } = params;
 
     const URL = import.meta.env.VITE_API_URL
 
@@ -16,5 +30,21 @@ export async function CheckLogin(username: string, password: string) {
     return response
 }
 
+export async function registerUser(params:RegisterUser) {
+    
+    const { username, email, password } = params;
+
+    const URL = import.meta.env.VITE_API_URL
+
+    const response = axios.post(`${URL}/users/register/`,
+    {
+        data: {
+            username, email, password
+        },
+        headers: { 'Content-Type': 'application/json' }
+    })
+
+return response
+}
 
 
